1610ae13135445c14f6f75474b721352ac63d6b5
Alec Trievel <atrievel@ceiamerica.com>
2018-11-13 15:50:33 -0500

    Update character selection scene

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -5,18 +5,19 @@ using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour
 {
-    public Button Char1, Char2, Char3;
+    public Button Char1, Char2, Char3, Char4;
 
     void Start()
     {
         Char1.onClick.AddListener(() => TaskOnClick(Char1));
         Char2.onClick.AddListener(() => TaskOnClick(Char2));
         Char3.onClick.AddListener(() => TaskOnClick(Char3));
+        Char4.onClick.AddListener(() => TaskOnClick(Char4));
     }
 
     void TaskOnClick(Button pressed)
     {
-        Debug.Log("You have selected the character: " + pressed.GetComponentInChildren<Text>().text);
+        Debug.Log("You have selected the button: " + pressed.name);
 
         /*
          * Set character to selected
