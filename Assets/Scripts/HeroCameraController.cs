07ab7c5fa1c932a1ae53f69a1969f0535d874f2a
John VanSickle <jwvansickle@icloud.com>
2018-11-12 19:19:14 -0500

    Add camera controller for hero to be followed

---
+++ Assets/Scripts/HeroCameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // LateUpdate is guaranteed after all items have had Update() been run
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
