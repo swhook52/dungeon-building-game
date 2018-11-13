0e99f0f39b3eea9ab55c5176d0bc32997816192a
John VanSickle <jwvansickle@icloud.com>
2018-11-12 19:13:51 -0500

    Add basic player and ground to scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical") * speed;
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement, ForceMode2D.Force);
+    }
+}
