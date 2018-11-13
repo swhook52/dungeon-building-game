ef5f2eaf6aa7cc682d054d0228797723dd71d9ec
Alec Trievel <atrievel@ceiamerica.com>
2018-11-13 12:02:49 -0500

    Add basic character select scene and script

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public Button Char1, Char2, Char3;
+
+    void Start()
+    {
+        Char1.onClick.AddListener(() => TaskOnClick(Char1));
+        Char2.onClick.AddListener(() => TaskOnClick(Char2));
+        Char3.onClick.AddListener(() => TaskOnClick(Char3));
+    }
+
+    void TaskOnClick(Button pressed)
+    {
+        Debug.Log("You have selected the character: " + pressed.GetComponentInChildren<Text>().text);
+
+        /*
+         * Set character to selected
+         * Setup game
+         * Hide UI
+         * Play game
+         */
+    }
+}
+
