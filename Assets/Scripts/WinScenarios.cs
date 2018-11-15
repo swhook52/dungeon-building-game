1b6b60f5c1fd365e2ceb06231767df13e5c0c057
Alec Trievel <atrievel@ceiamerica.com>
2018-11-15 11:18:05 -0500

    Add win scenario scene

---
+++ Assets/Scripts/WinScenarios.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScenarios : MonoBehaviour
+{
+    Text textComponent;
+
+    void Start()
+    {
+        textComponent = GetComponent<Text>();
+    }
+
+    public void SetLivesLabelText(float sliderValue)
+    {
+        var lives = Mathf.Round(sliderValue);
+        PlayerPrefs.SetFloat("lives", lives);
+
+        if (lives == 1)
+        {
+            textComponent.text = "Hero starts with " + lives.ToString() + " life";
+        }
+        else
+        {
+            textComponent.text = "Hero starts with " + lives.ToString() + " lives";
+        }
+    }
+
+    public void SetLootLabelText(float sliderValue)
+    {
+        var loot = Mathf.Round(sliderValue);
+        PlayerPrefs.SetFloat("loot", loot);
+
+        textComponent.text = "Hero needs " + loot.ToString() + " loot to win";
+    }
+
+    public void SaveWinScenarios()
+    {
+        //save win scenario vars to "local storage"
+        PlayerPrefs.Save();
+
+        Debug.Log("Lives is now set to: " + PlayerPrefs.GetFloat("lives").ToString());
+        Debug.Log("Loot is now set to: " + PlayerPrefs.GetFloat("loot").ToString());
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoBackToCharacterSelection()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
