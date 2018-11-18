713b26b0d223e39450a969f658db5504c8a8a886
Steven Hook <steven@hookscode.com>
2018-11-18 11:56:10 -0500

    Add DM currency regenerators

---
+++ Assets/Scripts/MonoBehaviors/DungeonMaster/CurrencyRegen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyRegen : MonoBehaviour {
+
+    public short RegenRate = 1;
+    public short RegenAmount = 1;
+
+    // Use this for initialization
+    void Start ()
+    {
+        InvokeRepeating("Regen", RegenRate, RegenRate);
+    }
+
+    private void Regen()
+    {
+        if (GameManager.instance)
+        {
+            GameManager.instance.dungeonMasterCurrency += RegenAmount;
+        }
+    }
+}
---
+++ Assets/Scripts/MonoBehaviors/DungeonMaster/DungeonMasterScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonMasterScore : MonoBehaviour
+{
+
+    private Text _textComponent;
+
+    public void Start()
+    {
+        _textComponent = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (GameManager.instance)
+        {
+            _textComponent.text = GameManager.instance.dungeonMasterCurrency.ToString();
+        }
+    }
+}
