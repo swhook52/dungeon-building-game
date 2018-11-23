1c4be2613c2e850d0c160f51ce6e84a2ac0286e3
Alec Trievel <atrievel@ceiamerica.com>
2018-11-23 10:10:36 -0500

    Add hero home and particle script

---
+++ Assets/Scripts/MonoBehaviors/LaunchParticlesOnCollide.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchParticlesOnCollide : MonoBehaviour
+{
+    public ParticleSystem _psystem;
+
+    void Awake()
+    {
+        _psystem = GetComponent<ParticleSystem>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _psystem.Play();
+        // end/next level here
+    }
+}
