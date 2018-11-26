7f976a7ee183fb49a173879a8d8587213a0ed478
Alec Trievel <atrievel@ceiamerica.com>
2018-11-26 13:20:33 -0500

    Add homing enemy script

---
+++ Assets/Scripts/HomingEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HomingEnemy : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5f;
+    public float rotationSpeed = 200f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)target.position - rb.position;
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, transform.up).z * -1;
+
+        rb.angularVelocity = rotateAmount * rotationSpeed;
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnCollisionEnter2D()
+    {
+        if(GameManager.instance)
+        {
+            //do real stuff here
+        }
+
+        Debug.Log("Player hit");
+    }
+}
